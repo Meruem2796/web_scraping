7da0cd0926df519cb6dbec1437b71bd8a959f760
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-04-07 12:13:11 +0200

    Exercise_3: Scraping Javascript generated content (data into HTML)

---
+++ code/exercise_3.py
@@ -0,0 +1,18 @@
+import json
+import scrapy
+
+
+class QuotesJSSpider(scrapy.Spider):
+    name = "quotes_js"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/js/"]
+
+    def parse(self, response):
+        # 1. Find the raw data inside the HTML
+        raw_quotes = response.xpath("//script").re_first(
+            r"var data = ((?s:\[.*?\]));"
+        )
+
+        # 2. With the raw data, convert it to Python and parse it
+
+        # 3. Don't forget we have pagination here too
